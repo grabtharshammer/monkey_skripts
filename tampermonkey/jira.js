b749db5245c046526efbd1e6484112f004676cb5
jon packer <grabtharshammer@users.noreply.github.com>
2016-05-27 12:47:59 -0600

    Create jira.js
    make external link open in new tab

---
+++ tampermonkey/jira.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         external link jira
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://bamboohr.atlassian.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	document.querySelector('.external-link').setAttribute('target','_blank');
+    
+})();
