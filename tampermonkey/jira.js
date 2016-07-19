2b2b8d970bfca6b2cb40f623b6248e8c2c08ed2a
jon packer <grabtharshammer@users.noreply.github.com>
2016-07-19 07:59:27 -0600

    Update jira.js
    up version of jira script

--- tampermonkey/jira.js
+++ tampermonkey/jira.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         external link jira
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://bamboohr.atlassian.net/*
